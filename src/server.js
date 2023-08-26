892c3e8ca77e4f19ebb28feb1f7fc34a8dbfefe1
Thiago silva <thiagonogueira.dev@gmail.com>
2023-08-26 01:28:23 -0300

    adicionado nodemon

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const route = require('./route')
 const path = require ('path')
+const { publicDecrypt } = require('crypto')
 
 const server = express()
 
 server.set ('view engine', 'ejs')
 
-server.use(express.static(public))
+server.use(express.static("public"))
 
-server.set('views', path.join(__dirname, 'viewsnpm s'))
+server.set('views', path.join(__dirname, 'views'))
 
 server.use(route)
 
